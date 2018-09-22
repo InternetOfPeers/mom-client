1327e239cf40ddd24caaeefeec436d13ac334ddf
Neurone <bertone.giuseppe@gmail.com>
2018-09-22 06:24:04 +0200

    Testing;

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -7,7 +7,6 @@ module.exports = {
 		app: "./src/index.js"
 	},
 	plugins: [
-		new CleanWebpackPlugin(["dist"]),
 		new HtmlWebpackPlugin({
 			title: "Production"
 		})
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,11 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
 	mode: "production",
-	devtool: "source-map"
+	devtool: "source-map",
+	plugins: [
+		new CleanWebpackPlugin(["dist"])
+	]
 });

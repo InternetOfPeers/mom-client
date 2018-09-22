1d1af677046aba823174dc943f305e9ea303d8a9
Neurone <bertone.giuseppe@gmail.com>
2018-09-22 03:59:45 +0200

    Testing;

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.png";
 
 function component() {
-  let element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+	let element = document.createElement("div");
+	element.innerHTML = _.join(["Hello", "webpack"], " ");
+	element.classList.add("hello");
 
-  // Add the image to our existing div.
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
+	// Add the image to our existing div.
+	var myIcon = new Image();
+	myIcon.src = Icon;
+	element.appendChild(myIcon);
 
-  return element;
+	return element;
 }
 
 document.body.appendChild(component());

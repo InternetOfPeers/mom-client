873e7448099ab793c3f37d333a63e24c11113e9a
Neurone <bertone.giuseppe@gmail.com>
2018-09-22 03:17:27 +0200

    Linter;

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png'
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello')
+  element.classList.add('hello');
 
   // Add the image to our existing div.
   var myIcon = new Image();
